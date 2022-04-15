6c1fd70fa186b9b36f57a802794a824a16f452da
godundmytro <godundmytro@gmail.com>
2022-04-15 13:08:35 +0300

    Handle read timeout exception

--- src/Runtime/Fpm/FpmApplication.php
+++ src/Runtime/Fpm/FpmApplication.php
@@ -3,29 +3,28 @@
 namespace Laravel\Vapor\Runtime\Fpm;
 
 use hollodotme\FastCGI\Client;
+use hollodotme\FastCGI\Exceptions\ReadFailedException;
 use hollodotme\FastCGI\SocketConnections\UnixDomainSocket;
+use Laravel\Vapor\Exceptions\SentryHandler;
 
 class FpmApplication
 {
     /**
      * The socket client instance.
-     *
      * @var \Hoa\Socket\Client
      */
     protected $client;
 
     /**
      * The FPM socket connection instance.
-     *
      * @var \Hoa\FastCGI\SocketConnections\UnixDomainSocket
      */
     protected $socketConnection;
 
     /**
      * Create a new FPM application instance.
-     *
-     * @param  \hollodotme\FastCGI\Client  $client
-     * @param  \Hoa\FastCGI\SocketConnections\UnixDomainSocket  $socketConnection
+     * @param \hollodotme\FastCGI\Client $client
+     * @param \Hoa\FastCGI\SocketConnections\UnixDomainSocket $socketConnection
      * @return void
      */
     public function __construct(Client $client, UnixDomainSocket $socketConnection)
@@ -36,14 +35,23 @@ class FpmApplication
 
     /**
      * Handle the given FPM request.
-     *
-     * @param  \Laravel\Vapor\Runtime\Fpm\FpmRequest  $request
+     * @param \Laravel\Vapor\Runtime\Fpm\FpmRequest $request
      * @return \Laravel\Vapor\Runtime\Fpm\FpmResponse
+     * @throws ReadFailedException
      */
     public function handle(FpmRequest $request)
     {
-        return new FpmResponse(
-            $this->client->sendRequest($this->socketConnection, $request)
-        );
+        try {
+            return new FpmResponse(
+                $this->client->sendRequest($this->socketConnection, $request)
+            );
+        } catch (ReadFailedException $e) {
+            SentryHandler::reportException($e, [
+                'uri' => $request->getRequestUri(),
+                'params' => $request->getParams(),
+            ]);
+
+            throw $e;
+        }
     }
 }
