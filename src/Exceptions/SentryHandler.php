6fb1263227e226968b4944eff2e08a5521b0734e
godundmytro <godundmytro@gmail.com>
2022-04-15 13:48:17 +0300

    Sentry logs

--- src/Exceptions/SentryHandler.php
+++ src/Exceptions/SentryHandler.php
@@ -4,6 +4,7 @@ namespace Laravel\Vapor\Exceptions;
 
 use Sentry\State\Scope;
 use function Sentry\captureException;
+use function Sentry\captureMessage;
 use function Sentry\configureScope;
 use function Sentry\init;
 
@@ -21,9 +22,12 @@ class SentryHandler
      */
     public static function init()
     {
+        fwrite(STDERR, 'Sentry DSN: '.config('vapor.sentry_dsn').' for env - '.$_ENV['APP_ENV'].PHP_EOL);
         if (config('vapor.sentry_dsn')) {
-            init(['dsn' => config('vapor.sentry_dsn')]);
+            init(['dsn' => config('vapor.sentry_dsn'), 'environment' => $_ENV['APP_ENV']]);
             self::$initialized = true;
+            fwrite(STDERR, 'Sentry initialized.'.PHP_EOL);
+            captureMessage('Sentry initialized.');
         }
     }
 
